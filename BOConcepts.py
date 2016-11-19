c7b26d419d476fbff9c207f61a8560d1b598c56f
tutecano1995 <tutecano1995@gmail.com>
2016-11-19 16:35:22 -0300

    Agregado xgb a BOConcepts.py

--- BOConcepts.py
+++ BOConcepts.py
@@ -9,6 +9,8 @@ import random
 import numpy as np
 import json
 import base64
+import xgboost as xgb
+
 
 def create_bag_of_centroids( wordlist, word_centroid_map ):
 	#
@@ -32,7 +34,7 @@ def create_bag_of_centroids( wordlist, word_centroid_map ):
 
 #Funcion muy cabeza que sirve para seleccionar aproximadamente 1500 de cada valor y que sirve solo para este caso, y que aproxima a una seleccion homogenea
 #Le otorgo probabilidad 1500/#reviews_con_esa_estrella dependiendo de que estrella toca
-#Para este caso el total de reviews por puntuación es: [ 37899,  21576,  30305,  56900, 260571]
+#Para este caso el total de reviews por puntuacion es: [ 37899,  21576,  30305,  56900, 260571]
 def elegir_filas_estrellas_homogeneas_2(estrellas):
     contador_estrellas=np.array([0]*5)
     cant_reviews=len(estrellas)
@@ -56,13 +58,13 @@ model = Word2Vec.load_word2vec_format("data/GoogleNews-vectors-negative300.bin",
 words = set()
 reviews = {}
 i = 0
-with open('train.csv','r') as train_file:
+with open('train_processed.csv','r') as train_file:
 	train_csv = csv.reader(train_file)
 	next(train_csv)
 	for row in train_csv:
-			reviewid = row[0]
-			predict = row[6]
-			text = row[9]
+			reviewid = row[1]
+			predict = row[7]
+			text = row[10]
 			text_words = text.split(" ")
 			if not(21<len(text_words)<223): continue
 			reviews[reviewid] = {}
@@ -73,8 +75,27 @@ with open('train.csv','r') as train_file:
 			for word in text_words:
 					words.add(word)
 
-print("train.csv cargado!")
+print("train_processed.csv cargado!")
 
+test_review = {}
+
+with open('test_processed.csv','r') as test_file:
+	test_csv = csv.reader(test_file)
+	next(test_csv)
+	for row in test_csv:
+			reviewid = row[1]
+			text = row[10]
+			text_words = text.split(" ")
+			if not(21<len(text_words)<223): continue
+			test_reviews[reviewid] = {}
+			test_reviews[reviewid]['text'] = text
+			if not i%10000: print(i)
+
+print("test_processed.csv cargado!")
+
+
+
+#Word embeddings
 selected_vectors = np.empty((0,300))	#La longitud de los vectores en el modelo es de 300
 word_index = []
 i = 0
@@ -109,39 +130,81 @@ for cluster in xrange(0,3):
 				cluster_words.append(word_centroid_map.keys()[i])
 	print (cluster_words)
 
-print("Hacemos BOCs")
+print("Hacemos BOCs de train")
 i = 0
-matriz_bocs = np.empty((0,num_clusters))
+train_matriz = []
 estrellas_bocs = []
 for reviewid, review in reviews.iteritems():
-	review['vec'] = create_bag_of_centroids(review['text'].split(" "),word_centroid_map)
+	review['vec'] = list(create_bag_of_centroids(review['text'].split(" "),word_centroid_map))
 	i+=1
 	if not i%10000:	print (i) 
-	matriz_bocs = np.vstack((matriz_bocs,review['vec']))
-	estrellas_bocs.append(review['pred'])
+	train_matriz.append(review['vec'])
+	estrellas_bocs.append(int(review['pred']))
+
+i = 0
+print("Hacemos BOCs de test")
+for reviewid, review in test_reviews.iteritems():
+	review['vec'] = list(create_bag_of_centroids(review['text'].split(" "),word_centroid_map))
+	i+=1
+	if not i%10000:	print (i) 
+
+print("No hacemos TSNE")
 
 #filas = elegir_filas_estrellas_homogeneas_2(estrellas_bocs)
-filas = elegir_filas_random(matriz_bocs)
-estrellas_seleccionadas = [int(estrellas_bocs[k]) for k in filas]
-matriz_red = matriz_bocs[filas]
+#estrellas_seleccionadas = [int(estrellas_bocs[k]) for k in filas]
+#matriz_red = matriz_bocs[filas]
 
-colourmap= {1:'#011f4b',2:'#03396c',3:'#005b96',4:'#6497b1',5:'#b3cde0'}
-colours = [colourmap[n] for n in estrellas_seleccionadas]
+#colourmap= {1:'#011f4b',2:'#03396c',3:'#005b96',4:'#6497b1',5:'#b3cde0'}
+#colours = [colourmap[n] for n in estrellas_seleccionadas]
 
-print("Y ahora reducimos con tsne")
-reduced_set =tsne(matriz_red,no_dims=2,initial_dims=20,perplexity=20.0,metric="cosine")
-
-star_hist=np.array([0]*5)
-for j in estrellas_seleccionadas:
-    star_hist[j-1]+=1
-
-Plot.scatter(reduced_set[:,0],reduced_set[:,1],c=colours_2,alpha=1,s=80,marker='.',lw=0)
-Plot.show()
+#print("Y ahora reducimos con tsne")
+#reduced_set =tsne(matriz_red,no_dims=2,initial_dims=20,perplexity=20.0,metric="cosine")
 
 
-estrellas_seleccionadas=np.array(estrellas_seleccionadas)
-with open("tsne_10k_random.json", 'w') as f:
-    json.dump(reduced_set,f,cls=NumpyEncoder)
 
-with open("tsne_10k_estrellas.json", 'w') as f:
-    json.dump(estrellas_seleccionadas,f,cls=NumpyEncoder)
+#star_hist=np.array([0]*5)
+#for j in estrellas_seleccionadas:
+#    star_hist[j-1]+=1
+
+#print(star_hist)
+
+#Plot.scatter(reduced_set[:,0],reduced_set[:,1],c=colours,alpha=1,s=80,marker='.',lw=0)
+#Plot.show()
+
+#estrellas_seleccionadas_save=np.array(estrellas_seleccionadas)
+
+#try:
+#	with open("tsne_10k_homo_cos_300cl.json", 'w') as f:
+#    	json.dump(reduced_set,f,cls=NumpyEncoder)
+
+#	with open("tsne_10k_homo_estrellas_300cl.json", 'w') as f:
+#    	json.dump(estrellas_seleccionadas_save,f,cls=NumpyEncoder)
+
+#except:
+#	print("No se pudieron guardar los modelos")
+
+train_matriz = numpy.matrix(train_matriz)
+dtrain = xgb.DMatrix(train_matriz)
+param = {'max_depth':10,'eta':0.2,'silent':0,'objective':'reg:linear'}
+dtrain = xgb.DMatrix(train_matriz,label = estrellas_bocs)
+bst = xgb.train(param,dtrain,num_boost_round = 3000)
+
+print("Guardamos los modelos")
+
+try: 
+	with open(str(num_clusters)+"_reg_xgb.csv") as f:
+		f_writer = csv.writer(f)
+		f_writer.writerow(['Id','Prediction'])
+		for reviewid,review in test_reviews.iteritems():
+			f_writer.writerow([reviewid,bst2.predict(xgb.DMatrix(review['vec']))[0]])
+
+	with open("boc_"+str(num_clusters)+"clu_clean_train.json",'w') as f:
+		json.dump(reviews,f)
+
+	with open("boc_"+str(num_clusters)+"clu_clean_test.json",'w') as f:
+		json.dump(test_reviews,f)
+
+except:
+	print("No se pudieron guardar los modelos")
+
+
